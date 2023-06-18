973bc5d9c7fb7adf50e52497ac27586f5809ac27
Gonzalo Fernandez <40607824+GFernandez3532@users.noreply.github.com>
2023-06-18 13:52:17 +1200

    End of section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -18,9 +18,21 @@ export default class ActivityStore {
     }
 
     get activitiesByDate() {
-        return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+        return Array.from(this.activityRegistry.values()).sort((a, b) =>
+            Date.parse(a.date) - Date.parse(b.date));
     }
 
+    get groupedActivities() {
+        return Object.entries(
+            this.activitiesByDate.reduce((activities, activity) => {
+                const date = activity.date;
+                
+                activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+
+                return activities;
+            }, {} as { [key: string]: Activity[] })
+        )
+    }
     loadActivities = async () => {
         this.setLoadingInitial(true);
         try {
@@ -54,9 +66,9 @@ export default class ActivityStore {
             try {
                 activity = await agent.Activities.details(id);
                 this.setActivity(activity);
-                runInAction(() =>this.selectedActivity = activity);                
+                runInAction(() => this.selectedActivity = activity);
                 this.setLoadingInitial(false);
-                
+
                 return activity;
 
             } catch (error) {
