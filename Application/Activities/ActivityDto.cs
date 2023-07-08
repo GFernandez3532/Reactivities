9b61c90444bc00cf67e09a05c7337665c5fa2909
Gonzalo Fernandez <40607824+GFernandez3532@users.noreply.github.com>
2023-07-08 22:18:53 +1200

    End of Section 14

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -1,6 +1,8 @@
-namespace Domain
+using Application.Activities.Profiles;
+
+namespace Application.Activities
 {
-    public class Activity
+    public class ActivityDto
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
@@ -8,9 +10,9 @@ namespace Domain
         public string Description { get; set; }
         public string Category { get; set; }
         public string City { get; set; }
-        public string Venue { get; set; } 
-
+        public string Venue { get; set; }
+        public string HostUserName { get; set; }
         public bool IsCancelled { get; set; }
-        public ICollection<ActivityAttendee> Attendees { get; set; } = new List<ActivityAttendee>();
+        public ICollection<Profile> Attendees { get; set; }
     }
 }
